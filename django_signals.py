40e0a392c55fd256b27290f33ad439bd3b77ca7e
raviva07 <ravivarma7939@gmail.com>
2025-12-16 18:06:14 +0530

    Update

--- django_signals.py
+++ django_signals.py
@@ -1,10 +1,1 @@
-# Custom Rectangle Class
 
-class Rectangle:
-    def __init__(self, length: int, width: int):
-        self.length = length
-        self.width = width
-    
-    def __iter__(self):
-        yield {"length": self.length}
-        yield {"width": self.width}
